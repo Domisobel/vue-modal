e7fd4ce6a4abcd09295b9f6db81d24db7ab0525e
Dominika <pimmortals@gmail.com>
2023-12-04 22:24:06 +0100

    ev

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,47 @@
 <template>
   <h1>{{ title }}</h1>
-  <ModalWindow/>
+  <ModalWindow :header="header " :text="text" theme="sale" />
 </template>
 
 <script>
-import ModalWindow from './components/ModalWindow.vue'
+import ModalWindow from "./components/ModalWindow.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: { ModalWindow },
+
   data() {
     return {
-      title: 'First vue app'
-      }
+      title: "First vue app",
+      header: 'Sign up',
+      text: 'Better faster'
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
     },
-    methods: {
-      handleClick() {
-        console.log(this.$refs.name)
-        this.$refs.name.classList.add('active')
-        this.$refs.name.focus()
-    }
-  }
-}
-
-
+  },
+};
 </script>
 
-<style scoped>
-h1 {
-  
-  display: flex;
-  justify-content: center;
-  border-bottom: 1px solid violet;
-  margin:  100px 600px;
+<style>
+#app {
+  text-align: center;
 }
-
+h1 {
+  display: inline-block;
+  text-align: center;
+  border-bottom: 1px solid violet;
+  margin: 100px 600px;
+}
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.sale h2 {
+  color: white;
+}
 </style>
--- src/components/ModalWindow.vue
+++ src/components/ModalWindow.vue
@@ -1,18 +1,26 @@
 <template>
     <div class="backdrop">
-        <div class="modal">
-            <p>modal content</p>
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: ['header', 'text', 'theme'],
+}
+</script>
 
 <style scoped>
 .modal {
     width: 400px;
     padding: 20px;
-    margin: 120px auto;
+    margin: 100px auto;
     background: white;
     border-radius: 10px;
+    text-align: center;
 }
 .backdrop {
     top: 0;
@@ -20,5 +28,7 @@
     background: rgba(0, 0, 0, 0.5); 
     width: 100%;
     height: 100%;
+    text-align: center;
+    
 }
 </style>
